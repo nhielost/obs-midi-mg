22d3e6518473b4c5a75c75e1b469f7e00f7f1556
George Walker <george@georgewwalker.com>
2025-12-16 18:39:19 -0500

    Revert "Remove unneeded logging"
    This reverts commit d101a15abcdddda8b852d90da11a4009d522a1f4.

--- src/mmg-utils.cpp
+++ src/mmg-utils.cpp
@@ -279,7 +279,13 @@ QJsonObject MMGJsonObject::toObject(const QByteArray &str)
 
 void MMGJsonObject::setJson(const QJsonObject &json)
 {
-	if (!editable) return;	
+	if (!editable) return;
+	
+	// Log the JSON being saved to this action
+	blog(LOG_INFO, "[MMGJsonObject] setJson() called - saving to action");
+	blog(LOG_INFO, "[MMGJsonObject]   Keys: %s", json.keys().join(", ").qtocs());
+	blog(LOG_INFO, "[MMGJsonObject]   JSON: %s", toString(json).constData());
+	
 	json_obj = json;
 }
 // End MMGJsonObject
